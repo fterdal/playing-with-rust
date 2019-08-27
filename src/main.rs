a6a8a0eb096cea68c2fe2163acd5c7e8783b6b8c
Finn <finnterdal@gmail.com>
2019-08-27 15:05:54 -0500

    add rand crate and updated rustc (the tutorial didnt mention extern crate thing)

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+use std::io;
+
 fn main() {
     let mut correct_num = 15;
+    let mut guess = String::new();
+    println!("Input your guess:");
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+    println!("You guessed {}", guess);
     loop {
         println!("{}", correct_num);
         correct_num -= 1;
