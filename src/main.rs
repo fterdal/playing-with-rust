2f80484d52e77a41f45a1740154f65f073192aa5
Finn <finnterdal@gmail.com>
2019-08-29 10:54:23 -0500

    how does clone work?

--- src/main.rs
+++ src/main.rs
@@ -23,31 +23,63 @@ fn main() {
     // pond.ducks.push(fred_the_duck);
     // pond.ducks.push(dorothy_the_duck);
     // println!("Duck Pond: {:#?}", pond);
-    println!("Duck Pond: {:#?}", get_duck_pond(fred_the_duck));
-    println!("Duck Pond: {:#?}", get_duck_pond(dorothy_the_duck));
+    let initial_duck_pond = DuckPond { ducks: Vec::new() };
+    // let after_one = get_new_duck_pond(initial_duck_pond, fred_the_duck);
+    // let after_two = get_new_duck_pond(after_one, dorothy_the_duck);
+    // println!("Duck Pond: {:#?}", after_two);
+    // println!("Duck Pond: {:#?}", get_new_duck_pond(dorothy_the_duck));
     // println!("Fred the Duck: {:#?}", fred_the_duck);
     // println!("Dorothy the Duck: {:#?}", dorothy_the_duck);
 }
 
-fn get_duck_pond(duck: Duck) -> DuckPond {
-    DuckPond { ducks: vec![duck] }
-}
+// fn get_new_duck_pond(duck_pond: DuckPond, duck: Duck) -> DuckPond {
+//     // let mut new_duck_pond = duck_pond.clone();
+//     // new_duck_pond.push(duck);
+//     // new_duck_pond
+// }
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct DuckPond {
     ducks: Vec<Duck>
 }
 
+// impl DuckPond {
+//     fn clone(&self) -> DuckPond {
+//         DuckPond { ducks: self.ducks.clone() }
+//     }
+// }
+
 #[derive(Debug)]
+#[derive(Clone)]
 struct Location {
     lat: f64,
     long: f64,
 }
+// impl Location {
+//     fn clone(&self) -> Location {
+//         Location {
+//             lat: self.lat.clone(),
+//             long: self.long.clone(),
+//         }
+//     }
+// }
 
 #[derive(Debug)]
+#[derive(Clone)]
 struct Duck {
     id: u32,
     name: String,
     age: u32,
     location: Location
 }
+// impl Duck {
+//     fn clone(&self) -> Duck {
+//         Duck {
+//             id: self.id,
+//             name: self.name.clone(),
+//             age: self.age,
+//             location: self.location.clone(),
+//         }
+//     }
+// }
