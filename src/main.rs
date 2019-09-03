7805d9c1aa108a8bc444ff68b40782913738a504
Finn <finnterdal@gmail.com>
2019-09-03 11:28:51 -0500

    create an object with a method that changes a property

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,20 @@
-mod duck_reducer;
-use duck_reducer::{Duck, DuckPond, Store};
+struct Counter {
+    count: u32
+}
+
+impl Counter {
+    pub fn get_count(&self) -> u32 {
+        self.count
+    }
+
+    pub fn increment(&mut self) {
+        self.count += 1;
+    }
+}
 
 fn main() {
-    // let fred = Duck::new(1, "Fred".to_string(), 12, 12.444, -32.555);
-    let dorothy = Duck::new(2, "Dorothy".to_string(), 8, 36.234566, -12.765785);
-    // let initial_duck_pond = DuckPond::new();
-    // println!("Initial Duck Pond: {:#?}", initial_duck_pond);
-    // let pond_with_ducks = initial_duck_pond
-    //     .add_duck(fred);
-        // .add_duck(dorothy);
-    // println!("Add Fred and Dorothy to Duck Pond: {:#?}", pond_with_ducks);
-    // println!("Initial Duck Pond (unmutated): {:#?}", initial_duck_pond);
-
-
-    // After dispatching Dorothy, the store still seems empty. Why is this?
-    // ANSWER: Because add_duck returns a new DuckPond, duh!!!
-    let mut store = Store::new();
-    // println!("{:#?}", store);
-    // store.get_state();
-    store.dispatch(dorothy);
-    // store.get_state();
-    println!("store get state: {:#?}", store.get_state());
-    println!("store: {:#?}", store);
+    let mut counter1 = Counter { count: 0 };
+    println!("Counter: {}", counter1.get_count());
+    counter1.increment();
+    println!("Counter: {}", counter1.get_count());
 }
